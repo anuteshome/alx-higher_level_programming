5abb58f8ad6ecbc633f1c3857c9096511700a88e
anuteshome <ananyateshome2@gmail.com>
2023-08-15 10:04:08 -0700

    2-arguments.js

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,10 +1,8 @@
 #!/usr/bin/node
-// Prints a message depending of the number of arguments passed
-
 if (process.argv.length === 2) {
   console.log('No argument');
 } else if (process.argv.length === 3) {
   console.log('Argument found');
-} else {
+} else if (process.argv.length > 3) {
   console.log('Arguments found');
 }
